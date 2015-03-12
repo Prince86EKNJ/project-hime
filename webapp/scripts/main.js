522281a8f947f48cd153d3ca57d1f5ced867fa6f
Edward Nicholes Jr <Prince86EKNJ@gmail.com>
2015-03-12 17:35:28 -0500

    Pipes and pumps

--- webapp/scripts/main.js
+++ webapp/scripts/main.js
@@ -1,1 +1,34 @@
 console.log("Hello Hime!");
+
+var buildPump = function(input, output) {
+	return function() {
+		output(input());
+	}
+}
+
+var pipe = function(input, output) {
+	input.out = output;
+}
+
+var systemTime = function() {
+	return new Date().getTime();
+}
+
+var buildRelativeValue = function(origin) {
+	return function(value) {
+		var result = value - origin;
+		arguments.callee.out(result);
+	}
+}
+
+console.out = function(value) {
+	console.log(value);
+}
+
+var now = systemTime();
+var relativeTime = buildRelativeValue(now);
+
+var pump = buildPump(systemTime, relativeTime);
+pipe(relativeTime, console.out);
+
+pump();
