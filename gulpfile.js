b8c381f1a9d84620cb9be8fc2df9bab945783948
Edward Nicholes Jr <GameDevFox@gmail.com>
2015-07-27 11:50:11 -0500

    Improved gulp file

--- gulpfile.js
+++ gulpfile.js
@@ -1,9 +1,33 @@
+var _ = require("lodash");
+
+var fs = require("fs");
 var gulp = require("gulp");
 
-gulp.task("default", function() {
-	gulp.watch("src/**/*.js", ["js-watch"]);
+gulp.task("build-scripts", function() {
+	console.log("Build scripts!!");
 });
 
-gulp.task("js-watch", function() {
-	console.log("Update!!");
+gulp.task("build-elements", function() {
+	console.log("Build elements!");
+});
+
+gulp.task("default", function() {
+
+	// Watch and build "src/*" directories
+	var sourceDirNames = fs.readdirSync("src");
+	_.each(sourceDirNames, function(dirName) {
+		var globPath = "src/"+dirName+"/**/*";
+		var taskName = "build-"+dirName;
+
+		// Create blank task
+		console.log("Watching \"src/"+dirName+"\" ...");
+		var watcher = gulp.watch(globPath);
+		watcher.on("change", function() {
+			if(_.has(gulp.tasks, taskName)) {
+				gulp.start(taskName);
+			} else {
+				console.log("Task \""+taskName+"\" has not been defined");
+			}
+		});
+	});
 });
