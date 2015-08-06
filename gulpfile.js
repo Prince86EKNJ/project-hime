8cbaad7f0b58eef6768834a051228b45637844e6
Edward Nicholes Jr <Prince86EKNJ@gmail.com>
2015-08-05 22:16:38 -0500

    Clean up gulp file

--- gulpfile.js
+++ gulpfile.js
@@ -4,12 +4,14 @@ var babel = require("gulp-babel");
 var babelify = require("babelify");
 var browserSync = require("browser-sync").create();
 var browserify = require("browserify");
+var del = require("del");
 var exorcist = require("exorcist");
 var files2Json = require("gulp-file-contents-to-json");
 var gulp = require("gulp");
 var jshint = require("gulp-jshint");
 var jshintStylish = require("jshint-stylish");
 var source = require("vinyl-source-stream");
+var symlink = require("gulp-symlink");
 var watchify = require("watchify");
 
 // Variables
@@ -18,74 +20,51 @@ var watchify = require("watchify");
 // Default task
 gulp.task("default", ["start", "watch"]);
 
-// Build tasks
-var buildScripts = function() {
+// Pre-build
+gulp.task("clean", function(done) {
+	del("webapp/scripts/**", done);
+});
 
-	// var bundler = watchify(browserify("src/scripts/index.js", watchify.args));
-
-	// // Babel transform
-	// bundler.transform(babelify.configure({
-	// 	sourceMapRelative: "webapp/scripts"
-	// }));
-
+gulp.task("lint", function() {
 	return gulp.src("src/scripts/**/*.js")
-		.pipe(babel({ modules: "amd" }))
+		.pipe(jshint({ esnext: true }))
+		.pipe(jshint.reporter(jshintStylish));
+});
+
+// Build
+gulp.task("link-libraries", ["clean"], function() {
+	return gulp.src(["bower_components/lodash/lodash.js", "bower_components/vue/dist/vue.js"])
+		.pipe(symlink(["webapp/scripts/lodash.js", "webapp/scripts/vue.js"]));	
+});
+
+gulp.task("build-scripts", ["lint", "link-libraries"], function() {
+	return gulp.src("src/scripts/**/*.js")
 //		.pipe(exorcist("webapp/scripts/main.js.map"))
+		.pipe(babel({ modules: "amd" }))
 //		.pipe(source("main.js"))
 		.pipe(gulp.dest("webapp/scripts"))
 		.pipe(browserSync.stream({ once: true }));
-};
-gulp.task("build-scripts", ["lint"], buildScripts);
-
-gulp.task("watch-scripts", function() {
-	gulp.watch("src/scripts/**/*.js", ["build-scripts"]);
 });
 
 gulp.task("build-elements", function() {
-	var stream =  gulp.src("src/elements/*")
+	return gulp.src("src/elements/*")
 		.pipe(files2Json("elements.json"))
-		.pipe(gulp.dest("webapp/data"));
-
-	browserSync.reload();
-
-	return stream;
+		.pipe(gulp.dest("webapp/data"))
+		.pipe(browserSync.stream({ once: true }));
 });
 
-var getTaskList = function(groupName) {
-	var tasks = _(gulp.tasks)
-		.filter(function(task) {
-			return _.startsWith(task.name, groupName+"-");
-		})
-		.map(function(task) {
-			return task.name;
-		}).value();
-	return tasks;
-}
-
-// Watch tasks
-gulp.task("watch-elements", ["build-elements"]);
+// Watch
+gulp.task("watch", function() {
+	gulp.watch("src/scripts/**/*.js", ["build-scripts"]);
+	gulp.watch("src/elements/**/*", ["build-elements"]);
+});
 
 // General tasks
-gulp.task("start", ["start-server"], function() {
-	gulp.start("start-webapp");
-});
+gulp.task("start", ["browserSync"]);
 
-gulp.task("start-server", function() {
-	gulp.start("server");
-});
+gulp.task("build", ["build-scripts", "build-elements"]);
 
-
-gulp.task("start-webapp", ["build"], function() {
-	gulp.start("browserSync");
-});
-
-var buildTasks = getTaskList("build");
-gulp.task("build", buildTasks);
-
-gulp.task("browserSync", function(done) {
-
-	// TODO: Move this
-	// bundler.on("update", buildScripts);
+gulp.task("browserSync", ["build"], function(done) {
 
 	browserSync.init({
 		files: ["webapp/styles/main.css"],
@@ -95,29 +74,4 @@ gulp.task("browserSync", function(done) {
 			baseDir: "./webapp"
 		}
 	}, done);
-});
-
-gulp.task("watch", function() {
-
-	var watchTasks = getTaskList("watch");
-	_.each(watchTasks, function(taskName) {
-		var dirName = taskName.substring(6);
-		console.log("Watching \"src/"+dirName+"\" ...");
-
-		var watcher = gulp.watch(globPath);
-		watcher.on("change", function() {
-			gulp.start(taskName);
-		});
-		var globPath = "src/"+dirName+"/**/*";
-	});
-});
-
-// gulp.task("server", function() {
-// 	kitsune.start();
-// });
-
-gulp.task("lint", function() {
-	return gulp.src("src/scripts/**/*.js")
-		.pipe(jshint({ esnext: true }))
-		.pipe(jshint.reporter(jshintStylish));
 });
