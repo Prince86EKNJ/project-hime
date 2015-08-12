2866a5ffd7795fef3baadf5f3fd159d205742615
Edward Nicholes Jr <Prince86EKNJ@gmail.com>
2015-08-12 08:08:23 -0500

    Tests using Mocha

--- gulpfile.js
+++ gulpfile.js
@@ -1,21 +1,19 @@
-var _ = require("lodash");
+console.log("Loading...");
 
 var babel = require("gulp-babel");
-var babelify = require("babelify");
 var browserSync = require("browser-sync").create();
-var browserify = require("browserify");
 var del = require("del");
 var exorcist = require("exorcist");
 var files2Json = require("gulp-file-contents-to-json");
 var gulp = require("gulp");
 var jshint = require("gulp-jshint");
 var jshintStylish = require("jshint-stylish");
+var mocha = require("gulp-mocha");
+var plumber = require("gulp-plumber");
 var source = require("vinyl-source-stream");
 var symlink = require("gulp-symlink");
-var watchify = require("watchify");
 
-// Variables
-// watchify.args.debug = true;
+console.log("Loaded!");
 
 // Default task
 gulp.task("default", ["start", "watch"]);
@@ -26,9 +24,16 @@ gulp.task("clean", function(done) {
 });
 
 gulp.task("lint", function() {
-	return gulp.src("src/scripts/**/*.js")
+	return gulp.src(["src/scripts/**/*.js", "test/**/*.js"])
 		.pipe(jshint({ esnext: true }))
 		.pipe(jshint.reporter(jshintStylish));
+});
+
+gulp.task("test", ["lint", "link-libraries"], function() {
+	return gulp.src("test/**/*.spec.js")
+		.pipe(plumber())
+		// .pipe(mocha({ reporter: "dot" }));
+		.pipe(mocha({ reporter: "spec" }));
 });
 
 // Build
@@ -37,10 +42,13 @@ gulp.task("link-libraries", ["clean"], function() {
 			"bower_components/lodash/lodash.js",
 			"bower_components/vue/dist/vue.js"
 		])
-		.pipe(symlink(["webapp/scripts/lodash.js", "webapp/scripts/vue.js"]));
+		.pipe(symlink([
+			"webapp/scripts/lodash.js",
+			"webapp/scripts/vue.js"
+		]));
 });
 
-gulp.task("build-scripts", ["lint", "link-libraries"], function() {
+gulp.task("build-scripts", ["test"], function() {
 	return gulp.src("src/scripts/**/*.js")
 //		.pipe(exorcist("webapp/scripts/main.js.map"))
 		.pipe(babel({
@@ -60,6 +68,7 @@ gulp.task("build-elements", function() {
 
 // Watch
 gulp.task("watch", function() {
+	gulp.watch("test/**/*.js", ["test"]);
 	gulp.watch("src/scripts/**/*.js", ["build-scripts"]);
 	gulp.watch("src/elements/**/*", ["build-elements"]);
 });
@@ -69,7 +78,7 @@ gulp.task("start", ["browserSync"]);
 
 gulp.task("build", ["build-scripts", "build-elements"]);
 
-gulp.task("browserSync", ["build"], function(done) {
+gulp.task("browserSync", ["build", "link-libraries"], function(done) {
 
 	browserSync.init({
 		files: ["webapp/styles/main.css"],
---
+++ test/sample.spec.js
@@ -0,0 +1,7 @@
+var expect = require("chai").expect;
+
+describe("My Test", function() {
+	it("should do something", function() {
+		expect(1).to.equal(1);
+	});
+});
